1e052dd19933c3e3a7fe3b7417cbe70b7f19f294
Dieter Reuter <dieter.reuter@me.com>
2016-06-14 17:03:29 +0200

    Cleanup kernel install

--- builder/chroot-script.sh
+++ builder/chroot-script.sh
@@ -41,13 +41,9 @@ apt-get update
 # # enable Docker systemd service
 # systemctl enable docker
 
-# install ODROID kernel
-
+# install ODROID C2 u-boot and kernel
 apt-get install -y u-boot-tools initramfs-tools
-
-# make the kernel package create a copy of the current kernel here
-touch /boot/uImage
-apt-get install -y linux-image-c2 || apt-get install -f
+apt-get install -y linux-image-c2
 ls -al /boot/*
 
 # set device label and version number
